676841daa3bfa2379e9eb7f9da1b4d0041f0cf6e
fayezabuhajar <abuhajarfayez@gmail.com>
2024-10-16 20:46:55 +0300

    Save changes before updating Angular

--- clint/src/app/members/member-detalis/member-detalis.component.ts
+++ clint/src/app/members/member-detalis/member-detalis.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-member-detalis',
   standalone: true,
-  imports: [],
+  imports: [TabsModule],
   templateUrl: './member-detalis.component.html',
   styleUrl: './member-detalis.component.css'
 })
-export class MemberDetalisComponent {
+export class MemberDetalisComponent implements OnInit{
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  member?: Member;
 
+  ngOnInit(): void {
+    this.loadMember()
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('userName');
+    if(!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => this.member = member
+    })
+  }
 }
