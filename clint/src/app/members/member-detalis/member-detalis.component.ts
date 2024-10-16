6b7a2a2aa2f57bc53d32264a5619f126762ebed2
fayezabuhajar <abuhajarfayez@gmail.com>
2024-10-16 21:33:51 +0300

    Commit changes before updating Angular

--- clint/src/app/members/member-detalis/member-detalis.component.ts
+++ clint/src/app/members/member-detalis/member-detalis.component.ts
@@ -3,11 +3,12 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detalis',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, GalleryModule],
   templateUrl: './member-detalis.component.html',
   styleUrl: './member-detalis.component.css'
 })
@@ -15,6 +16,7 @@ export class MemberDetalisComponent implements OnInit{
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
   member?: Member;
+  images: GalleryItem[] = [];
 
   ngOnInit(): void {
     this.loadMember()
@@ -24,7 +26,12 @@ export class MemberDetalisComponent implements OnInit{
     const username = this.route.snapshot.paramMap.get('userName');
     if(!username) return;
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member;
+          member.photos.map(p => {
+            this.images.push(new ImageItem({src: p.url, thumb:p.url}))
+          })
+      }
     })
   }
 }
